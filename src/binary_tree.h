397cfde66716f3e8e8280da0d94ecedfdfaf4963
ZhuJiang <cococolo@users.noreply.github.com>
2015-05-14 23:26:55 +0800

    Update binary_tree.h
    new binary_tree.h

--- src/binary_tree.h
+++ src/binary_tree.h
@@ -5,186 +5,193 @@
 
 using namespace std;
 
-template <typename CompareValue>
-class binary_tree
+template<typename Comparable>
+class BinarySearchTree
 {
 public:
-    binary_tree();
-    binary_tree(const binary_tree & rhs)
-        : root(NULL)
-    {
-        root = clone(rhs.root);
-    }
-    ~binary_tree();
+	BinarySearchTree() : root(NULL)
+	{
+		debug("BinarySearchTree constructer");
+	}
+	BinarySearchTree(const BinarySearchTree & rhs);
+	~BinarySearchTree( )
+	{
+		makeEmpty();
+		debug("BinarySearchTree desconstructor");
+	}
 
-    const CompareValue & findMin() const
-    {
 
-    }
+	const Comparable & findMin() const
+	{
+		return findMin(root)->element;
+	}
+	const Comparable & findMax() const
+	{
+		return findMax(root)->element;
+	}
 
-    const CompareValue & findMax() const
-    {
+	bool contains(const Comparable & x) const
+	{
+		return contains(x, root);
+	}
 
-    }
-
-    bool contains(const CompareValue & x) const
-    {
-        return contains(x, root);
-    }
-
-    bool isEmpty() const
-    {
-
-    }
-
-    void makeEmpty()
-    {
-
-    }
-
-    void insert(const CompareValue & x)
-    {
-        insert(x, root);
-    }
-
-    void remove(CompareValue & x)
-    {
-        remove(x, root);
-    }
-
-    binary_tree & operator=(const binary_tree & rhs)
-    {
-
-    }
+	bool isEmpty() const
+	{
+		return (root == NULL) ? true :false;
+	}
+	void printTree() const
+	{
+		debug("print tree");
+		printNode(root);
+	}
+	void makeEmpty()
+	{
+		makeEmpty(root);
+	}
+	void insert(const Comparable & x)
+	{
+		insert(x, root);
+	}
+	void remove(const Comparable & x)
+	{
+		remove(x, root);
+	}
+	BinarySearchTree & operator=( const BinarySearchTree & rhs );
 
 private:
-    struct BinaryNode
-    {
-        CompareValue *element;
-        BinaryNode *left;
-        BinaryNode *right;
+	struct BinaryNode
+	{
+		Comparable element;
+		BinaryNode *left;
+		BinaryNode *right;
 
-        BinaryNode(CompareValue & x, BinaryNode *ltree, BinaryNode *rtree)
-        : element(x), left(ltree), right(rtree)
-        {}
-    };
+		BinaryNode( const Comparable & theElement, BinaryNode *lt, BinaryNode *rt )
+		: element(theElement), left(lt), right(rt)
+		{
+			//debug("BinaryNode constructer!");
+		}
 
-    BinaryNode *root;
+	};
 
-    void insert(const CompareValue & x, BinaryNode * & t)
-    {
-        if(t == NULL)
-        {
-            t = new BinaryNode(x, NULL, NULL);
-        }
-        else if(x < t->element)
-        {
-            insert(x, t->left);
-        }
-        else if(x > t->element)
-        {
-            insert(x, t->right);
-        }
-    }
+	BinaryNode *root;
 
-    void remove(const CompareValue & x, BinaryNode * & t)
-    {
-        if(t == NULL)
-        {
-            return;
-        }
-        if(x < t->element)
-        {
-            remove(x, t->left);
-        }
-        else if(x > t->element)
-        {
-            remove(x, t->right);
-        }
-        else if(t->left != NULL && t->right != NULL)
-        {
-            t->element = findMin(t->right)->element;
-            remove(t->element, t->right);
-        }
-        else
-        {
-            BinaryNode *oldNode = t;
-            t = (t->left != NULL) ? t->left : t->right;
-            delete oldNode;
-        }
-    }
+	//use * &t, can not use *t, new will have no effect
+	void insert( const Comparable & x, BinaryNode * &t)
+	{
+		if(t == NULL)
+		{
+			t = new BinaryNode(x, NULL, NULL);
+		}
+		else if(x <= t->element)
+		{
+			insert(x, t->left);
+		}
+		else if(x > t->element)
+		{
+			insert(x, t->right);
+		}
+	}
+	void remove(const Comparable & x, BinaryNode * &t)
+	{
+		if(t == NULL)
+		{
+			return;
+		}
 
-    BinaryNode * findMin(BinaryNode *t) const
-    {
-        if(t == NULL)
-        {
-            return NULL;
-        }
-        if(t->left == NULL)
-        {
-            return t;
-        }
-        return findMin(t->left);
-    }
+		if(x <= t->element)
+		{
+			remove(x, t->left);
+		}
+		else if(t->left != NULL && t->right != NULL)
+		{
+			t->element = findMin(t->right)->element;
+			remove(t->element, t->right);
+		}
+		else
+		{
+			BinaryNode *oldNode = t;
+			t = (t->left != NULL) ? t->left : t->right;
+		}
+	}
+	BinaryNode * findMin( BinaryNode *t ) const
+	{
+		if(t == NULL)
+		{
+			return NULL;
+		}
+		if(t->left == NULL)
+		{
+			return t;
+		}
+		return findMin(t->left);
+	}
+	BinaryNode * findMax( BinaryNode *t ) const
+	{
+		if(t == NULL)
+		{
+			return NULL;
+		}
 
-    BinaryNode * findMax(BinaryNode *t) const
-    {
-        if(t != NULL)
-        {
-            while(t->right != NULL)
-            {
-                t = t->right;
-            }
-        }
-        return t;
-    }
+		if(t->right == NULL)
+		{
+			return t;
+		}
+		return findMax(t->right);
+	}
+	bool contains( const Comparable & x, BinaryNode *t ) const
+	{
+		if(t == NULL)
+		{
+			return false;
+		}
+		else if(x < t->element)
+		{
+			return contains(x, t->left);
+		}
+		else if(x > t->element)
+		{
+			return contains(x, t->right);
+		}
+		else
+		{
+			return true;
+		}
+	}
+	void makeEmpty( BinaryNode *t)
+	{
+		if(t != NULL)
+		{
+			printf("%d ", t->element);
+			makeEmpty(t->left);
+			makeEmpty(t->right);
+			delete t;
+		}
+		t = NULL;
+	}
+	void printTree( BinaryNode *t, ostream & out ) const;
+	BinaryNode * clone( BinaryNode *t ) const;
 
-    bool contains(const CompareValue & x, BinaryNode *t) const
-    {
-        if(t == NULL)
-        {
-            return false;
-        }
-        else if(x < t->element)
-        {
-            return contains(x, t->left);
-        }
-        else if(x > t->element)
-        {
-            return contains(x, t->right);
-        }
-    }
 
-    void makeEmpty(BinaryNode * & t)
-    {
-        if(t != NULL)
-        {
-            makeEmpty(t->left);
-            makeEmpty(t->right);
-            delete t;
-        }
-        t = NULL;
-    }
 
-    void printTree(BinaryNode *t, ostream & out) const
-    {
+	void printNode(BinaryNode *t) const
+	{
+		if(t == NULL)
+		{
+			return;
+		}
+		printf("%d ", t->element);
 
-    }
+		if(t->left != NULL)
+		{
+			printNode(t->left);
+		}
 
-    BinaryNode * clone(BinaryNode *t) const
-    {
-        if(t == NULL)
-        {
-            return NULL;
-        }
-        else
-        {
-            return new BinaryNode(t->element, clone(t->left), clone(t->right));
-        }
-    }
+		if(t->right != NULL)
+		{
+			printNode(t->right);
+		}
+	}
 };
-
-
 
 
 
